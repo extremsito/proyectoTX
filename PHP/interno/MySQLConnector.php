63eb04aae8966cdbf252d8d2aa5e2e1a9ac89063
xafoco <150243674+xafoco@users.noreply.github.com>
2023-11-20 13:12:31 +0100

    Delete PHP directory

--- PHP/interno/MySQLConnector.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "root";
-    $password = "proyectoTX";
-    $database = "proyectoteoxavi";
-    $port = 3306;
-
-    $mysql = mysqli_connect($host, $user, $password, $database, $port);
-?>
